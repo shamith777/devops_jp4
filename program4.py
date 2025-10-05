30ef6dbfce58959caffc6e360551de8b1ac15a16
shamith777 <shamithramdurg302@gmail.com>
2025-10-05 12:52:28 +0530

    Saving file to local

--- program4.py
+++ program4.py
@@ -2,6 +2,9 @@ a = int(input("Enter first Number ="))
 b = int(input("Enter second number ="))
 add = a+b
 subtract = a-b
+multiply = a*b
+division = a/b
 print("Addition =",add)
 print("Subtraction =",subtract)
-
+print("Multiply =",multiply)
+print("Division =",division)
